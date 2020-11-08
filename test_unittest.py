70bc09fb959d92846eb8e4122dcc2e4ba6f9b909
Mikhail Isaev <danluck@mail.ru>
2020-11-08 21:32:09 +0300

    Switch to pytest

--- test_unittest.py
+++ test_unittest.py
@@ -1,17 +1,13 @@
 import inc_dec    # The code to test
-import unittest   # The test framework
 
-class Test_TestIncrementDecrement(unittest.TestCase):
-    def test_increment(self):
-        self.assertEqual(inc_dec.increment(3), 4)
-        self.assertEqual(inc_dec.increment(65535), 65536)
-        self.assertEqual(inc_dec.increment(4294967295), 4294967296)
-        self.assertEqual(inc_dec.increment(0xFFFFFFFFFFFFFFFE), 0xFFFFFFFFFFFFFFFF)
-        self.assertEqual(inc_dec.increment(0xFFFFFFFFFFFFFFFF), 18446744073709551616)
+def test_increment():
+    assert inc_dec.increment(3) == 4
+    assert inc_dec.increment(3) == 4
+    assert inc_dec.increment(65535) == 65536
+    assert inc_dec.increment(4294967295) == 4294967296
+    assert inc_dec.increment(0xFFFFFFFFFFFFFFFE) == 0xFFFFFFFFFFFFFFFF
+    assert inc_dec.increment(0xFFFFFFFFFFFFFFFF) == 18446744073709551616
 
-    def test_decrement(self):
-        self.assertEqual(inc_dec.decrement(3), 2)
-        self.assertEqual(inc_dec.decrement(-3), -4)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_decrement():
+    assert inc_dec.decrement(3) == 2
+    assert inc_dec.decrement(-3) == -4
